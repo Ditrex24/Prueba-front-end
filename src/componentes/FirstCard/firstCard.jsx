ef3a2921eef28a89dd4ca7990ba036d7cda76923
Ditrex24 <vasquezsergiodg@gmail.com>
2024-03-17 09:51:55 -0400

    segunda lamina

---
+++ src/componentes/FirstCard/firstCard.jsx
@@ -0,0 +1,27 @@
+
+import '../FirstCard/firstCard.css'; // Asegúrate de que la ruta del archivo CSS sea correcta
+
+
+
+const FirstCard = () => {
+  return (
+    <div className="firtsCard">
+      <div className="textFirst">
+        <h1>Loren ipsum dolor sit amet</h1>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+    diam nonumy eirmod tempor invidunt ut labore et dolore
+    magna aliquyam erat, sed diam voluptua. At vero eos et
+    accusam et justo duo dolores et ea rebum. Stet clita kasd
+    gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+    amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+    sed diam nonumy eirmod tempor invidunt ut labore et dolore
+    magna aliquyam erat, sed diam voluptua. At vero eos et
+    accusam et justo duo dolores et ea rebum. Stet clita kasd
+    gubergren, no sea takimata sanctus est Lorem ipsum dolor sit </p>
+      </div>
+      <div className="imageFirstCard"></div>
+    </div>
+  );
+}
+
+export default FirstCard;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import Header from './componentes/header.jsx';
+import FirstCard from './componentes/FirstCard/firstCard.jsx';
 
 
 const root = createRoot(document.getElementById('root'));
@@ -10,6 +11,8 @@ const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
+    <FirstCard/>
+    
     <App />
   </React.StrictMode>
 );
